d41460c94c4b01c140bf8ecc9b03961acf542f7d
isikjon <isikjandev@gmail.com>
2025-09-03 22:12:54 +0500

    101% 03.09.2025

--- main.py
+++ main.py
@@ -39,5 +39,5 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8102) 
+    uvicorn.run(app, host="0.0.0.0", port=8000) 
     
